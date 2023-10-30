cbf551d404f0cb8a450120b0f670e5ec066c44b0
Vincent Odhiambo <ovincent7949@gmail.com>
2023-10-30 21:18:46 +0300

    exclude vendor dir from ignored telescope folders

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -36,7 +36,7 @@ return {
             ['<C-Up>'] = actions.cycle_history_prev,
           },
         },
-        file_ignore_patterns = { '.git/', 'node_modules/', 'vendor/', 'virtualenv/' },
+        file_ignore_patterns = { '.git/', 'node_modules/', 'virtualenv/' },
       },
       extensions = {
         live_grep_args = {
