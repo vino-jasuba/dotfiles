ccf7b20a0ddab8c1377e35616d415c8b8c7988d0
Vincent Odhiambo <ovincent7949@gmail.com>
2023-12-17 08:26:45 +0300

    add syntax aware expand selection

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -68,6 +68,15 @@ return {
     rainbow = {
       enable = true,
     },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-space>',
+        node_incremental = '<c-space>',
+        scope_incremental = '<s-space>',
+        node_decremental = '<M-space>',
+      },
+    },
     textobjects = {
       select = {
         enable = true,
@@ -77,6 +86,35 @@ return {
           ['af'] = '@function.outer',
           ['ia'] = '@parameter.inner',
           ['aa'] = '@parameter.outer',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
         },
       },
     },
